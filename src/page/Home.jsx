8fd33fbe19a7c8e9d0e9b309de30810cceaadae5
sdorado2 <sdorado2@gmail.com>
2023-03-21 22:27:45 -0400

    removed strict mode to lower the amount of calls to the api

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -7,19 +7,20 @@ const Home = () => {
   const api_key = process.env.REACT_APP_API_KEY;
   // const url = "#";
 
+  useEffect(() => {
+    getWeather("New York");
+  }, []);
+
   const getWeather = async (searchTerm) => {
     const response = await axios
       .get(
         `http://api.openweathermap.org/data/2.5/forecast?q=${searchTerm},us&appid=${api_key}`
       )
       .then((res) => res.data);
+
     console.log("Return response:", response);
     setWeather(response);
   };
-
-  useEffect(() => {
-    getWeather("New York");
-  }, []);
 
   return (
     <div>
@@ -27,9 +28,7 @@ const Home = () => {
       <div>
         <Search searchLocation={getWeather} />
       </div>
-      <h1>
-        {weather.city.name},{weather.city.country}
-      </h1>
+      <h1>{/* {weather.city.name},{weather.city.country} */}</h1>
       {/* <h2>Temp : {weather.list[0].main.temp}</h2> */}
     </div>
   );
