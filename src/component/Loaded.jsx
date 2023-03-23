f525d83d6dcc38564c954cd278b3b78dd67d7ac4
sdorado2 <sdorado2@gmail.com>
2023-03-23 11:16:05 -0400

    mapping temperatures from api call

--- src/component/Loaded.jsx
+++ src/component/Loaded.jsx
@@ -7,7 +7,13 @@ const Loaded = ({ weather }) => {
       <h1>
         {weather.city.name},{weather.city.country}
       </h1>
-      <h2>Temp : {weather.list[0].main.temp}</h2>
+      <h2>Temperature : {weather.list[0].main.temp}</h2>
+      <h2>At : {weather.list[0].dt_txt}</h2>
+      <span>
+        {weather.list.map((elem) => (
+          <h2>{elem.main.temp}</h2>
+        ))}
+      </span>
     </div>
   );
 };
