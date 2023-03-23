1968029fbf31eb75b2f730b30705608768a59532
sdorado2 <sdorado2@gmail.com>
2023-03-23 19:49:55 -0400

    creating a toggle to change temperatures

--- src/component/Loaded.jsx
+++ src/component/Loaded.jsx
@@ -3,16 +3,60 @@ import Celsius from "./Celsius";
 import Fahrenheit from "./Fahrenheit";
 
 const Loaded = ({ weather }) => {
+  // const defaultDisplay = displayInFahrenheit();
+
+  // const displayInFahrenheit = () => {
+  //   return (
+  //     <div className="spanTest">
+  //       {weather.list.map((elem) => (
+  //         <div className="testing">
+  //           <h2>
+  //             At : {elem.dt_txt} <hr /> Temp :{" "}
+  //             <Fahrenheit temp={elem.main.temp} />
+  //           </h2>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   );
+  // };
+
+  // const displayInCelsius = () => {
+  //   return (
+  //     <div className="spanTest">
+  //       {weather.list.map((elem) => (
+  //         <div className="testing">
+  //           <h2>
+  //             At : {elem.dt_txt} <hr /> Temp : <Celsius temp={elem.main.temp} />
+  //           </h2>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   );
+  // };
+
+  const temperatureNow = () => {
+    return (
+      <div>
+        <h1>
+          {weather.city.name},{weather.city.country}
+        </h1>
+        <h2>Temperature : {weather.list[0].main.temp}</h2>
+        <h2>At : {weather.list[0].dt_txt}</h2>
+      </div>
+    );
+  };
+
   console.log("It is loaded");
+
   return (
     <div>
-      <h1>
-        {weather.city.name},{weather.city.country}
-      </h1>
-      <h2>Temperature : {weather.list[0].main.temp}</h2>
-      <h2>At : {weather.list[0].dt_txt}</h2>
+      {temperatureNow()}
       <h2>Forecast</h2>
-      <div className="spanTest">
+      <div className="convertTemp">
+        <div>Fahrenheit</div>
+        <div>Celsius</div>
+      </div>
+      {/* <div className="spanTest">
         {weather.list.map((elem) => (
           <div className="testing">
             <h2>
@@ -21,7 +65,7 @@ const Loaded = ({ weather }) => {
             </h2>
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
