01fc205d135124182a0accf287a2ec0ee880dadb
sdorado2 <sdorado2@gmail.com>
2023-03-24 16:09:56 -0400

    modified dateTime inside the display in fahrenheit

--- src/component/DisplayInFahrenheit.jsx
+++ src/component/DisplayInFahrenheit.jsx
@@ -4,22 +4,21 @@ import Fahrenheit from "./Fahrenheit";
 const DisplayInFahrenheit = ({ weather }) => {
   const MILLISECONDS = 1000;
   let dateObj;
+  let dateTime;
   let date;
   let hrs;
+
   return (
     <div className="forecast">
       {weather.list.map((elem) => (
-        
+        // console.log(elem.dt)
+        // dateObj = new Date(elem.dt);
         <div className="hrForecast">
-          {
-          dateObj = new Date(elem.dt * {MILLISECONDS});
-          date = dateObj.toDateString();
-          hrs = dateObj.toLocaleTimeString();
-          }
           <h2>
-            At : {elem.dt_txt} <hr /> Temp :{" "}
-            {}
-            <Fahrenheit temp={elem.main.temp} />
+            At : {new Date(elem.dt * 1000).toLocaleTimeString()}
+            <hr />
+            Date :{new Date(elem.dt * 1000).toDateString()}
+            <hr /> Temp : <Fahrenheit temp={elem.main.temp} />
           </h2>
           <p>
             {elem.weather[0].main}
