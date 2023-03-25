f150b0e7615fb75c1b3ea3148432568d1f1ccb8b
sdorado2 <sdorado2@gmail.com>
2023-03-24 21:20:24 -0400

    Adjustments to Display in Fahrenheit; minor house cleaning

--- src/component/DisplayInFahrenheit.jsx
+++ src/component/DisplayInFahrenheit.jsx
@@ -5,15 +5,15 @@ const DisplayInFahrenheit = ({ weather }) => {
   return (
     <div className="forecast">
       {weather.list.map((elem) => (
-        // console.log(elem.dt)
-        // dateObj = new Date(elem.dt);
         <div className="hrForecast">
-          <h2>
+          <h4>
             At :{" "}
             {new Date(elem.dt * 1000).toLocaleTimeString("en-US", {
               timeZone: "America/New_York",
             })}
             <hr />
+          </h4>
+          <h2>
             Date :{new Date(elem.dt * 1000).toDateString()}
             <hr /> Temp : <Fahrenheit temp={elem.main.temp} />
           </h2>
